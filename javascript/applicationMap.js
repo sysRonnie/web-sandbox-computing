fc003eae42c50e1d0f2c8eed5fd4054861d9f509
sysronnie <sysronnie@gmail.com>
2024-07-09 13:48:43 -0400

    Feat: Loading Screen.FIXED ZOOM..NAVBAR  Timeline: Details and Comb through. Cloud function for message

--- javascript/applicationMap.js
+++ javascript/applicationMap.js
@@ -62,7 +62,7 @@ function applyNodeStyles(selection, currentPage) {
     }
     
     selection.select('circle')
-        .attr('r', d => isSubTrack(d.id) ? 45 : 55)
+        .attr('r', d => isSubTrack(d.id) ? 35 : 45)
         .attr('stroke', d => isSubTrack(d.id) ? '#00B3FF' : '#FFFFFF')
         .attr('stroke-width', 3)
         .attr('fill', d => d.id === currentPage ? '#635DFE' : '#121212');
@@ -71,7 +71,7 @@ function applyNodeStyles(selection, currentPage) {
 function handleMouseOver(event, d) {
     d3.select(this)
         .select('circle')
-        .attr('r', 60)
+        .attr('r', 55)
         .attr('fill', '#EC5C5C')
         .attr('stroke', '#FFFFFF')
         .attr('stroke-width', 3);
@@ -99,7 +99,7 @@ function updateNodeStyles(svg, currentPage) {
 
 function createVisualization(data) {
     const width = window.innerWidth * 1.1;
-    const height = window.innerHeight * 0.6;
+    const height = window.innerHeight * 0.7;
     const currentPage = document.querySelector('.visible').id;
 
     const svg = d3.select('#visualization')
@@ -110,7 +110,7 @@ function createVisualization(data) {
     const simulation = d3.forceSimulation(data.nodes)
         .force('link', d3.forceLink(data.links).id(d => d.id).distance(100))
         .force('charge', d3.forceManyBody().strength(-300))
-        .force('center', d3.forceCenter(width / 2, height / 2.5))
+        .force('center', d3.forceCenter(width / 2, height / 2))
         .force('x', d3.forceX().strength(0.1).x(d => {
             if (d.id === 'PageA') return width / 2;
             if (d.id === 'PageB-1L') return width / 2 - 200;
